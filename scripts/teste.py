82733cdf9a2600a021c3cde615c7c194a11bb354
CleissonVieira <cleisson.raimundi@gmail.com>
2024-09-15 23:02:28 -0300

    estrutura inicial + datasets

---
+++ scripts/teste.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+url_dataset = 'https://raw.githubusercontent.com/CleissonVieira/fake-reviews-bert-ptbr/main/datasets/portuguese/data-set-teste.csv'
+yelp_df = pd.read_csv(url_dataset)
